95ae8d40c13a98b004f8ac7a2b0724e037c9b634
Matt Copperwaite <mattcopp@gmail.com>
2012-03-26 18:41:37 +0100

    Added GPLv3 license to files

--- downloader.py
+++ downloader.py
@@ -1,3 +1,20 @@
+"""
+ This file is part of Python-Downloader.
+
+ Python-Downloader is free software: you can redistribute it and/or modify
+ it under the terms of the GNU General Public License as published by
+ the Free Software Foundation, either version 3 of the License, or
+ (at your option) any later version.
+
+ Python-Downloader is distributed in the hope that it will be useful,
+ but WITHOUT ANY WARRANTY; without even the implied warranty of
+ MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ GNU General Public License for more details.
+
+ You should have received a copy of the GNU General Public License
+ along with Python-Downloader.  If not, see <http://www.gnu.org/licenses/>.
+"""
+
 from urllib2 import urlopen as download_file
 from time import time as time_now
 from urlparse import urlparse
